fc7d86bb45c884f7b0b77de6e003b4322d507295
Thom Low <thomthomlow@gmail.com>
2024-03-17 12:34:07 +0800

    starting

---
+++ Portfolio/Pages/Homepage/HomePage.jsx
@@ -0,0 +1,9 @@
+export function HomePage() {
+    return (
+        <div>
+            Home page
+        </div>
+    )
+}
+
+// export default HomePage;
---
+++ Portfolio/Pages/Year1/Year1.jsx
@@ -0,0 +1,9 @@
+export function Year1Page() {
+    return (
+        <div>
+            Year1Page page
+        </div>
+    )
+}
+
+// export default Year1Page;
